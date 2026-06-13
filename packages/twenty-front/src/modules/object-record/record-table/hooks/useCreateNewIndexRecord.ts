9a9ba41f7e5035c15cd5adacb8b876b045d4a123
Youssef-Abdelsalam-2005 <youssef.abdelsalam1234@gmail.com>
2026-06-13 19:32:51 +0400

    Allow opening new records with required fields

--- packages/twenty-front/src/modules/object-record/record-table/hooks/useCreateNewIndexRecord.ts
+++ packages/twenty-front/src/modules/object-record/record-table/hooks/useCreateNewIndexRecord.ts
@@ -1,9 +1,7 @@
 import { useSidePanelMenu } from '@/side-panel/hooks/useSidePanelMenu';
 import { useOpenRecordInSidePanel } from '@/side-panel/hooks/useOpenRecordInSidePanel';
 import { type EnrichedObjectMetadataItem } from '@/object-metadata/types/EnrichedObjectMetadataItem';
-import { formatFieldMetadataItemAsFieldDefinition } from '@/object-metadata/utils/formatFieldMetadataItemAsFieldDefinition';
 import { getLabelIdentifierFieldMetadataItem } from '@/object-metadata/utils/getLabelIdentifierFieldMetadataItem';
-import { isFieldMetadataRequired } from '@/object-metadata/utils/isFieldMetadataRequired';
 import { useBuildRecordInputFromRLSPredicates } from '@/object-record/hooks/useBuildRecordInputFromRLSPredicates';
 import { useCreateOneRecord } from '@/object-record/hooks/useCreateOneRecord';
 import { recordGroupDefinitionsComponentSelector } from '@/object-record/record-group/states/selectors/recordGroupDefinitionsComponentSelector';
@@ -12,10 +10,8 @@ import { recordIndexOpenRecordInState } from '@/object-record/record-index/state
 import { recordIndexRecordIdsByGroupComponentFamilyState } from '@/object-record/record-index/states/recordIndexRecordIdsByGroupComponentFamilyState';
 import { useUpsertRecordsInStore } from '@/object-record/record-store/hooks/useUpsertRecordsInStore';
 import { useBuildRecordInputFromFilters } from '@/object-record/record-table/hooks/useBuildRecordInputFromFilters';
-import { isFieldValueEmpty } from '@/object-record/record-field/ui/utils/isFieldValueEmpty';
 import { type ObjectRecord } from '@/object-record/types/ObjectRecord';
 import { canOpenObjectInSidePanel } from '@/object-record/utils/canOpenObjectInSidePanel';
-import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
 import { useAtomComponentFamilyStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentFamilyStateCallbackState';
 import { useAtomComponentSelectorValue } from '@/ui/utilities/state/jotai/hooks/useAtomComponentSelectorValue';
 import { useAtomComponentStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateValue';
@@ -25,7 +21,6 @@ import { useCallback } from 'react';
 import { AppPath } from 'twenty-shared/types';
 import { findByProperty, isDefined } from 'twenty-shared/utils';
 import { v4 } from 'uuid';
-import { t } from '@lingui/core/macro';
 import { useNavigateApp } from '~/hooks/useNavigateApp';
 
 type UseCreateNewIndexRecordProps = {
@@ -64,7 +59,6 @@ export const useCreateNewIndexRecord = ({
   });
 
   const { upsertRecordsInStore } = useUpsertRecordsInStore();
-  const { enqueueErrorSnackBar } = useSnackBar();
 
   const navigate = useNavigateApp();
 
@@ -89,34 +83,6 @@ export const useCreateNewIndexRecord = ({
         ...recordInputFromFilters,
         ...recordInput,
       };
-
-      const missingRequiredField = objectMetadataItem.fields.find((field) => {
-        if (
-          !isFieldMetadataRequired(field) ||
-          isDefined(field.defaultValue) ||
-          field.isUIReadOnly
-        ) {
-          return false;
-        }
-
-        const fieldDefinition = formatFieldMetadataItemAsFieldDefinition({
-          field,
-          objectMetadataItem,
-        });
-
-        return isFieldValueEmpty({
-          fieldDefinition,
-          fieldValue: mergedRecordInput[field.name],
-        });
-      });
-
-      if (isDefined(missingRequiredField)) {
-        enqueueErrorSnackBar({
-          message: t`Fill required fields before creating this record.`,
-        });
-
-        return;
-      }
 
       const recordIndexOpenRecordIn = store.get(
         recordIndexOpenRecordInState.atom,
@@ -198,7 +164,6 @@ export const useCreateNewIndexRecord = ({
       buildRecordInputFromRLSPredicates,
       buildRecordInputFromFilters,
       createOneRecord,
-      enqueueErrorSnackBar,
       navigate,
       objectMetadataItem,
       openRecordInSidePanel,
